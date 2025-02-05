e5dfa663dd968daf5ddaa91c354454d7f343f19a
Yotam loewenbach <yotam.loewenbach@logz.io>
2025-02-05 14:01:44 +0700

    Disable `CGO`, eliminating the dependency on the lambda env glibc version

--- logzio-lambda-extensions-logs/build-zip.sh
+++ logzio-lambda-extensions-logs/build-zip.sh
@@ -1,4 +1,4 @@
-env GOOS=linux GOARCH=amd64 go build -o bin/extensions/logzio-lambda-extensions-logs main.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/extensions/logzio-lambda-extensions-logs main.go
 chmod +x bin/extensions/logzio-lambda-extensions-logs
 cd bin
 zip -r extension.zip extensions/
